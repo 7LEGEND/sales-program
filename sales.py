7168c361acd86a48c65ce9d024a2373dca6ccc7d
c10wnn <98447283+c10wnn@users.noreply.github.com>
2022-05-17 15:37:03 +0200

    Add files via upload

---
+++ sales.py
@@ -0,0 +1,12 @@
+def main():
+ month_list = ['Juanary', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
+ #define a dictionary that will have the montha name and month sales
+ dic={}
+ for month_name in month_list:
+  dic[month_name] = float(input('Enter the sales for '+month_name))
+ print('Total sales:', sum(dic.values()))
+ avg = sum(dic.values()) / len(dic)
+ print('Average sales: %.2f ' % avg)
+ print('Highest sales :', max(dic, key=dic.get))
+ print('Lowest sales : ', min(dic, key=dic.get))
+main()
